6d956895f688eddbde29002e2c01f93d864721ec
shaid30044 <kaona30044@gmail.com>
2023-10-20 14:44:21 +0600

    add delete method on cart products

--- index.js
+++ index.js
@@ -106,6 +106,7 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
+    // await client.close();
   }
 }
 run().catch(console.dir);
