03ff5d7e85880f389e0c2c93095a532abe9100ed
shaid30044 <kaona30044@gmail.com>
2023-10-21 09:32:55 +0600

    comment await client.connect() and await client.db()

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const productCollection = client.db("productDB").collection("product");
     const cartCollection = client.db("cartDB").collection("cart");
@@ -101,7 +101,7 @@ async function run() {
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
